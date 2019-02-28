ffa9a6657f54471be0ba73115c51a085d1c8b8dc
Kevin Stiefel <kevsti@yahoo.de>
2019-02-28 17:27:28 +0100

    fix some small errors

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ h = 1000
 l = 1920
 field = np.zeros((h, l))  # << NO.
 
-
 viewportX = list(range(l))
 viewportY = list(range(h))
 
@@ -74,11 +73,12 @@ ARCHER = CreepType(2, 100, 75, 200, 25, 900, 45, 8, "Unite_Archer")
 GIANT = CreepType(1, 140, 50, 0, 40, 2000, 200, 10, "Unite_Siege")
 
 
-#class Distance: << (Vector2.kt : Distance) - IGNORED/Useless
+# class Distance: << (Vector2.kt : Distance) - IGNORED/Useless
 
 class Vector2:
     """Vector2.kt : Vector2
     """
+
     def __int__(self, x=0, y=0):
         self.x = x
         self.y = y
@@ -95,44 +95,43 @@ class AbstractPlayer:
 class Player(AbstractPlayer):
     """Player.kt : Player
     """
-    
+
     def __init__(self):
         self.queenUnit = None
         self.enemyPlayer = None
         self.activeCreeps = []
-        
+
         # original line:
         # 'var health by nonNegative<Player>(100).andAlso { if (score >= 0) score = it }'
         self.health = None
         self.score = -2  # := self.health ?
         self.gold = Constants.STARTING_GOLD
         self.goldPerTurn = 0  # << this looks kinda bad ..
-        #self.hud = None
-        
-    #def getExpectedOutputLines: return 2
-    #def fixOwner(self, player): raise NotImplementedError()
-    #def printObstacleInit(self, obstacle): raise NotImplementedError()
-    #def printObstaclePerTurn(self, obstacle): raise NotImplementedError()
-    
+        # self.hud = None
+
+    # def getExpectedOutputLines: return 2
+    # def fixOwner(self, player): raise NotImplementedError()
+    # def printObstacleInit(self, obstacle): raise NotImplementedError()
+    # def printObstaclePerTurn(self, obstacle): raise NotImplementedError()
+
     def allUnits(self):
         return self.activeCreeps + [self.queeUnit]  # TODO not sure <<
-    
+
     def checkQueenHealth(self):
         self.queenUnit.health = self.health  # << really ?? double bookkeeping here.
         if self.health == 0:
             self.deactivate("Dead queen")
-        #self.hud.update()
+        # self.hud.update()
 
-    def kill(reason):
+    def kill(self, reason):
         self.score = -1
         self.deactivate(reason)
-    
-    
+
 
 class FieldObject:
     """Characters.kt : FieldObject
     """
-    
+
     def __int__(self):
         self.location = None
         self.radius = 0
@@ -156,26 +155,27 @@ class Unit(FieldObject):
     @abstractmethod
     def damage(self, damageAmount):
         pass
-    
+
 
 class Queen(Unit):
     """Characters.kt : Queen
     """
-    
+
     def __int__(self, owner):
         super().__init__(owner)  # TODO
         self.mass = Constants.QUEEN_MASS
-    
+
     def moveTowards(self, target):
         """
         param target - Vector2
         """
         self.location = self.location.towards(target, Constants.QUEEN_SPEED)
-    
+
     def damage(self, damageAmount):
         if (damageAmount <= 0):
             return
-        owner.health = max(0, owner.health - damageAmount)  # TODO CHECK: maybe just ignore owner, and use self here?!?
+        self.owner.health = max(0,
+                                self.owner.health - damageAmount)  # TODO CHECK: maybe just ignore owner, and use self here?!?
         # self.health = max(0, owner.health - damageAmount)
 
 
@@ -219,25 +219,33 @@ class Creep(Unit):
 class GiantCreep(Creep):
     """Characters.kt : GiantCreep
     """
-    
+
     def __int__(self, owner):
         super().__init__(owner, KNIGHT)
         self.obstacles = []  # type: Obstacle
-        
+
     def move(self, frames):
         """
         params frames - double, ?
         """
         # TODO (how/where to) get current obstacles ? - set externally?
-        opp_structures = filter(lambda struct:struct != None and struct.owner and struct.owner == self.owner.enemyPlayer)
-        target = min(opp_structures, key=lambda struct:struct.location.distanceTo(self.location))
-        
+        opp_structures = filter(
+            lambda struct: struct != None and struct.owner and struct.owner == self.owner.enemyPlayer)
+        target = min(opp_structures, key=lambda struct: struct.location.distanceTo(self.location))
+
         self.location = self.location.towards(target)
 
     def dealDamage(self):
         pass  # TODO
-    
+
+
 class KnightCreep(Creep):
+
+    def dealDamage(self):
+        pass
+
+    def move(self, frames):
+        pass
 
     def __int__(self, owner):
         super().__init__(owner, KNIGHT)
@@ -297,12 +305,13 @@ class KnightCreep(Creep):
 ###  from Structures.kt
 """
 
+
 def Obstacle(FieldObject):
     """Structures.kt : Obstacle
     """
     nextObstacleId = 0
-    
-    def __init__(maxMineSize, initialGold, initialRadius, initialLocation):
+
+    def __init__(self, maxMineSize, initialGold, initialRadius, initialLocation):
         super().__init__()  # TODO CHECK call
         self.maxMineSize = maxMineSize
         self.initialGold = initialGold
@@ -310,51 +319,54 @@ def Obstacle(FieldObject):
         self.initialLocation = initialLocation
         self.obstacleId = Obstacle.nextObstacleId
         Obstacle.nextObstacleId += 1
-        
+
         self.mass = 0
-        assert(initialGold >= 0)
+        assert (initialGold >= 0)
         self.gold = initialGold
-        #self.obstacleImage = None
+        # self.obstacleImage = None
         self.radius = 0
         self.location = initialLocation
         self.area = np.pi * self.radius ** 2
         self.structure = None
-        
+
     def setMine(self, owner):
         self.structure = Mine(self, owner, 1)
-    
+
     def setTower(self, owner, health):
         self.structure = Tower(self, owner, 0, health)
-    
+
     def setBarracks(self, owner, creepType):
         self.structure = Mine(self, owner, creepType)
-    
+
+
 class Structure:
     def __init__(self):
         self.owner = None
         self.obstacle = None
-        
-    def act():
+
+    def act(self):
         """ fun act(): Boolean // return true if the Structure should be destroyed
         """
         raise NotImplementedError()
-    
+
+
 class Mine(Structure):
     def __init__(self, obstacle, owner, incomeRate):
         super().__init__()
         self.obstacle = obstacle
         self.owner = owner
         self.incomeRate = incomeRate
-        
+
     def act(self):
-        cash = min(incomeRae, self.obstacle.gold)
+        cash = min(self.incomeRae, self.obstacle.gold)
         self.owner.goldPerTuren += cash
         self.owner.gold += cash
         self.obstacle.gold -= cash
         if self.obstacle.gold <= 0:
             return True  # mine exhausted >> destroy
         return False
-    
+
+
 class Tower(Structure):
     def __init__(self, obstacle, owner, attackRadius, health):
         super().__init__()
@@ -362,44 +374,44 @@ class Tower(Structure):
         self.owner = owner
         self.attackRadius = attackRadius
         self.health = health
-        
+
         self.attackTarget = None  # (> FieldObject)
-    
-    
+
     def damageCreep(self, targetCreep):
         shotDistance = self.target.location.distanceTo(self.obstacle.location) - self.obstacle.radius
         differenceFromMax = self.attackRadius - shotDistance
         damage = Constants.TOWER_CREEP_DAMAGE_MIN + int(differenceFromMax / Constants.TOWER_CREEP_DAMAGE_CLIMB_DISTANCE)
         self.target.damage(damage)
-        
-        
+
     def act(self):
         closestEnemy = None
         if self.owner.enemyPlayer.activeCreeps:
-            closestEnemy = min(self.owner.enemyPlayer.activeCreeps, key=lambda it: it.lcation.distanceTo(self.obstacle.location))
+            closestEnemy = min(self.owner.enemyPlayer.activeCreeps,
+                               key=lambda it: it.lcation.distanceTo(self.obstacle.location))
         enemyQueen = self.owner.enemyPlayer.queenUnit
-        
-        if closestEnemy != None and closestEnemy.location.distanceTo(obstacle.location) < attackRadius:
+
+        if closestEnemy != None and closestEnemy.location.distanceTo(self.obstacle.location) < self.attackRadius:
             self.attackTarget = closestEnemy
             self.damageCreep(self.attackTarget)
-        elif enemyQueen.location.distanceTo(obstacle.location) < attackRadius:
+        elif enemyQueen.location.distanceTo(self.obstacle.location) < self.attackRadius:
             self.attackTarget = enemyQueen
             self.damageQueen(self.attackTarget)
-            
+
         # duplication in self.attackTarget ..
-        
+
         else:
             self.attackTarget = None
-        
+
         self.healt -= Constants.TOWER_MELT_RATE
         self.attackRadius = int(((self.health * Constants.TOWER_COVERAGE_PER_HP + self.obstacle.area) / np.pi) ** 0.5)
-        
+
         if self.health <= 0:
             return True  # Tower destroyed (time/attack)
         return False
-    
+
+
 class Barracks(Structure):
-    
+
     def __init__(self, obstacle, owner, creepType):
         super().__init__()
         self.obstacle = obstacle
@@ -408,17 +420,18 @@ class Barracks(Structure):
         self.progressMax = creepType.buildTime
         self.progress = 0
         self.isTraining = False
-        
+
         # 'var onComplete: () -> Unit = {}  ## << ??    
-    
+
     def act(self):
         if self.isTraining:
             self.progress += 1
-            if self.progress = self.progressMax:
+            if self.progress == self.progressMax:
                 self.progress = 0
                 self.isTraining = False
                 self.onComplete()  # create a creep ?
         return False
+
 
 # End: Structures.kt
 
@@ -428,78 +441,77 @@ class Barracks(Structure):
 """
 from random import Random
 
+
 class AbstractReferee:  # TODO find, fill or ignore
     pass
-    
+
+
 class Referee(AbstractReferee):
     def __init__(self, params):
         super().__init__()
         self.obstacles = []
         self.theGameManager.maxTurns = 200  # < why not Constants.XXX here?
-       
+
         # Now, this gets interesting.
         # are Kotlin and Python RNGs 'compatible'?!?
-        self.theRandom = Random(int(params['seed']) if params['seed'] else Random()
-   
+        self.theRandom = Random(int(params['seed'])) if params['seed'] else Random()
+
         if self.gameManager.leagueLevel == 1:
             Leagues.mines = False
-            Leagues.fixedIncome = Constants.WOOD_FIXED_INCOME
-            Leagues.towers = False
-            Leagues.giants = False
-            Leagues.obstacles = Constants.OBSTACLE_PAIRS.sample()
+        Leagues.fixedIncome = Constants.WOOD_FIXED_INCOME
+        Leagues.towers = False
+        Leagues.giants = False
+        Leagues.obstacles = Constants.OBSTACLE_PAIRS.sample()
         elif self.gameManager.leagueLevel == 2:
-            Leagues.mines = False
-            Leagues.fixedIncome = Constants.WOOD_FIXED_INCOME
-            Leagues.obstacles = Constants.OBSTACLE_PAIRS.sample()
+        Leagues.mines = False
+        Leagues.fixedIncome = Constants.WOOD_FIXED_INCOME
+        Leagues.obstacles = Constants.OBSTACLE_PAIRS.sample()
         elif self.gameManager.leagueLevel == 3:
         else:
-            Leagues.queenHp = Constants.QUEEN_HP.sample() * Constants.QUEEN_HP_MULT
-        
+        Leagues.queenHp = Constants.QUEEN_HP.sample() * Constants.QUEEN_HP_MULT
+
         self.gameManager.frameDuration = 750  # another magic number, can also be ignored, i guess.
-        
+
         self.gameManager.players[0].enemyPlayer = self.gameManager.players[1]
-        self.gameManager.players[01.enemyPlayer = self.gameManager.players[0]
+        self.gameManager.players[01.
+        enemyPlayer = self.gameManager.players[0]
         self.gameManager.players[1].isSecondPlayer = True
         for p in self.gameManager.players:
             p.health = Leagues.queenHp
-        
+
         self.obstacles = buildMap()
-        
+
         for activePlaye, invert in zip(self.gameManger.activePlayers, [False, True]):
             spawnDistance = 200  # Magic number
-            if invert:
-                corner = Vector2(Constants.WORLD_WIDTH - spawnDistance, Constants.WORLD_HEIGHT - spawnDistance)
-            else:
-                corner = Vector2(spawnDistance, spawnDistance)
-            activePlayer.queenUnit = Queen(activePlayer)
-            activePlayer.queenUnit.location = corner
-        
+        if invert:
+            corner = Vector2(Constants.WORLD_WIDTH - spawnDistance, Constants.WORLD_HEIGHT - spawnDistance)
+        else:
+            corner = Vector2(spawnDistance, spawnDistance)
+        activePlayer.queenUnit = Queen(activePlayer)
+        activePlayer.queenUnit.location = corner
+
         self.fixCollisions(self.allEntities())
-        
-        #for p in self.gameManager.activePlayers:
+
+        # for p in self.gameManager.activePlayers:
         #    p.hud.update()
         #    p.sendInputLine(len(self.obstacles))
         #    for o in self.obstacles:
         #        p.printObstacleInit(o)
-        
+
         return params
-    
-    
+
     def allEntities(self):
         return [u for p in self.gameManager.players for u in p.allUnits()] + self.obstacles
-        
-    
+
     def gameTurn(self, turn):
         def sendGameStates():
             pass  # TODO Referee.kt lines 95 .. 117 missing
             # only needed if stdin/out interaction is necessary.
-            
+
         def processPlayerActions():
             pass  # FIXME Referee.kt lines 119 .. 167
             # TODO most interesting stuff happens here..
-            
-        
-        
+
 
 """
 ### MapBuilding.kt
@@ -508,8 +520,5 @@ class Referee(AbstractReferee):
 # TODO ...
 
 
-
-        
 if __name__ == '__main__':
-    pass    
-    
+    pass
