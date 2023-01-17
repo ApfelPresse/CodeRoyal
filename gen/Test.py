48c1862688cb0be645290596370bf409cb694f6d
KevinStiefel <kevin.stiefel@deutschebahn.com>
2023-01-17 19:58:49 +0100

    working

--- gen/Test.py
+++ gen/Test.py
@@ -1,60 +1,71 @@
 import unittest
-from multiprocessing import Pool
-
-import gymnasium as gym
-import numpy as np
 
 from gen.genetic import genetic_algorithm
+from gen.tree import predict
 
 
 class Test(unittest.TestCase):
 
-    def test_irgendwas(self):
-        env = gym.make('MountainCar-v0')
-        observation, info = env.reset()
+    def test_xor(self):
+        def score_xor(args):
+            tree, it = args
+            predictions = []
+            predictions.append(predict(inp=[0, 1], tree=tree) == 1)
+            predictions.append(predict(inp=[1, 0], tree=tree) == 1)
+            predictions.append(predict(inp=[1, 1], tree=tree) == 0)
+            predictions.append(predict(inp=[0, 0], tree=tree) == 0)
+            return sum(predictions), tree
 
-        nodes = observation.shape[0]
+        nodes = 2
+        n_pop = 10
         config = {
-            "n_iter": 5,
-            "n_pop": 100,
+            "n_iter": 50,
+            "n_pop": 10,
             "r_cross": 0.4,
-            "elite_size": 10,
+            "elite_size": int(n_pop * 0.1),
             "r_mut": 1 / nodes,
             "nodes": nodes,
-            "max_score": 100000000,
-            "p1_possible_values": np.linspace(0, 1, num=70),
+            "max_score": 4,
+            "p1_possible_values": [0, 1],
             "p2_possible_values": list(range(nodes)),
             "p3_possible_values": list(range(nodes)),
-            "p4_possible_values": np.linspace(0, 200, num=20),
-            "p5_possible_values": list(range(env.action_space.n)),  # labels
+            "p4_possible_values": [0, 1],
         }
-        with Pool(10) as pool:
-            pool = None
-            best, score = genetic_algorithm(Test.mountain_car, config, pool)
+        # with Pool(10) as pool:
+        #     pool = None
+        best, score = genetic_algorithm(score_xor, config, None)
         print('Done!')
         print(best)
         print(f"Score {score}")
-        Test.mountain_car((best, 0))
-        print()
 
-    @staticmethod
-    def mountain_car(args):
-        tree, it = args
+    def test_max(self):
+        def score_xor(args):
+            tree, it = args
+            predictions = []
+            predictions.append(predict(inp=list([0, 1, 2]), tree=tree) == 2)
+            predictions.append(predict(inp=list([1, 2, 3]), tree=tree) == 3)
+            predictions.append(predict(inp=list([2, 3, 4]), tree=tree) == 4)
+            predictions.append(predict(inp=list([3, 4, 5]), tree=tree) == 5)
+            predictions.append(predict(inp=list([4, 5, 6]), tree=tree) == 6)
+            return sum(predictions), tree
 
-        env = gym.make('MountainCar-v0')
-        observation, info = env.reset()
-
-        score = 0
-        terminated, truncated = False
-        while not terminated and not truncated:
-            # for _ in range(1000):
-
-            action = env.action_space.sample()  # agent policy that uses the observation and info
-            observation, reward, terminated, truncated, info = env.step(action)
-
-            score += reward
-            if terminated or truncated:
-                observation, info = env.reset()
-
-        env.close()
-        return score, tree
+        nodes = 10
+        n_pop = 100
+        config = {
+            "n_iter": 10000,
+            "n_pop": 10,
+            "r_cross": 0.6,
+            "elite_size": int(n_pop * 0.1),
+            "r_mut": 1 / nodes,
+            "nodes": nodes,
+            "max_score": 5,
+            "p1_possible_values": [0, 1, 2, 3, 4, 5, 6],
+            "p2_possible_values": list(range(nodes)),
+            "p3_possible_values": list(range(nodes)),
+            "p4_possible_values": [2, 3, 4, 5, 6],
+        }
+        # with Pool(10) as pool:
+        #     pool = None
+        best, score = genetic_algorithm(score_xor, config, None)
+        print('Done!')
+        print(f"Score {score}")
